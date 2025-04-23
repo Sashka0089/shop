78d67621acab3b8711d27e600a6b99318ceadeff
Alexander <Xbe55iX@yandex.ru>
2025-04-23 21:11:51 +0300

    состояние готово, осталось смену иконок

--- src/components/Faq/FaqInfo.tsx
+++ src/components/Faq/FaqInfo.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import "./FaqInfo.scss";
 
 interface IFaq {
@@ -29,26 +29,52 @@ const faqInfo = () => {
     },
   ];
 
+  const [answer, setAnswer] = useState<number[]>([]);
 
-
+  function toogle(id: number) {
+    setAnswer((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...answer, id]
+    );
+  }
 
   return (
     <div className="faqInfo">
       {faqInfo.map((faq) => (
-        <div key={faq.id}>
-          <div className="faqInfo__block">
-            <div className="faqInfo__question">{faq.question}</div>
+        <div key={faq.id} className="faqInfo__block">
+          <div className="faqInfo__question">{faq.question}</div>
 
-            <svg className="faqInfo__icon" width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12.3852 12.2477L20.9052 3.72768L21.7891 4.61157L13.2691 13.1316L21.6577 21.5201L20.6834 22.4943L12.2949 14.1058L4.11144 22.2892L3.22756 21.4054L11.411 13.2219L3.00578 4.81667L3.98 3.84245L12.3852 12.2477Z" fill="white" />
-            </svg>
-            <svg className="faqInfo__icon" width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12.9509 12.387L25 12.387V13.637L12.9509 13.637L12.9509 25.5002H11.5731L11.5731 13.637H0L0 12.387H11.5731V0.500244H12.9509V12.387Z" fill="white" />
-            </svg>
+          <svg
+            onClick={() => toogle(faq.id)}
+            className="faqInfo__icon"
+            width="25"
+            height="26"
+            viewBox="0 0 25 26"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.3852 12.2477L20.9052 3.72768L21.7891 4.61157L13.2691 13.1316L21.6577 21.5201L20.6834 22.4943L12.2949 14.1058L4.11144 22.2892L3.22756 21.4054L11.411 13.2219L3.00578 4.81667L3.98 3.84245L12.3852 12.2477Z"
+              fill="white"
+            />
+          </svg>
+          <svg
+            onClick={() => toogle(faq.id)}
+            className="faqInfo__icon"
+            width="25"
+            height="26"
+            viewBox="0 0 25 26"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.9509 12.387L25 12.387V13.637L12.9509 13.637L12.9509 25.5002H11.5731L11.5731 13.637H0L0 12.387H11.5731V0.500244H12.9509V12.387Z"
+              fill="white"
+            />
+          </svg>
 
+          {answer.includes(faq.id) && (
             <div className="faqInfo__answer">{faq.answer}</div>
-          </div>
-
+          )}
         </div>
       ))}
     </div>
